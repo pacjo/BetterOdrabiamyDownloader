1a48b43c60d9061b8b37508b8d7f04a8498e1462
pacjo <kamil.kroliczek2004@gmail.com>
2022-01-07 16:58:46 +0100

    some cleanup, now saves files directly to book folder

--- BetterOdrabiamyDownloader.py
+++ BetterOdrabiamyDownloader.py
@@ -12,7 +12,7 @@ path = os.path.dirname(os.path.abspath(filename))
 
 user=input('Podaj E-Mail: ')
 password=getpass.getpass(prompt='Podaj hasło: ')
-bookid=input('Podaj ID cionszki: ')
+bookid=input('Podaj ID książki: ')
 
 try:
     rpost = requests.post(url=('https://odrabiamy.pl/api/v2/sessions'), json=({"login": f"{user}", "password": f"{password}"})).content
@@ -20,7 +20,7 @@ try:
 except:
     print('Niepoprawny e-mail lub hasło. A może nie masz premium?')
     exit()
-            
+
 def download_page(token, page, bookid):
     rget = requests.get(url=f'https://odrabiamy.pl/api/v2/exercises/page/premium/{page}/{bookid}', headers={'user-agent':'new_user_agent-huawei-142','Authorization': f'Bearer {token}'}).content.decode('utf-8')
     lists=json.loads(rget).get('data')
@@ -29,16 +29,15 @@ def download_page(token, page, bookid):
 
     if not os.path.exists(f'{path}/{name}-{bookid}'):
         os.makedirs(f'{path}/{name}-{bookid}')
-        os.makedirs(f'{path}/{name}-{bookid}/{page}')
 
     for exercise in lists:
-        if not os.path.exists(f'{path}/{name}-{bookid}/{page}'):
-            os.makedirs(f'{path}/{name}-{bookid}/{page}')
+        if not os.path.exists(f'{path}/{name}-{bookid}'):
+            os.makedirs(f'{path}/{name}-{bookid}')
         number=exercise.get('number')
-        file=open(f'{path}/{name}-{bookid}/{page}/index.html', 'a+', encoding='utf-8')
+        file=open(f'{path}/{name}-{bookid}/{page}.html', 'a+', encoding='utf-8')
         file.write(f'<head><meta charset="UTF-8"></head>\n<a style="color:red; font-size:25px;">Zadanie {number}</a><br>\n{exercise.get("solution")}<br>')
         file.close()
-        
+
 rget = requests.get(url=f'https://odrabiamy.pl/api/v1.3/ksiazki/{bookid}').content.decode('utf-8')
 if rget == '{"'+f'error":"Couldn\'t find Book with \'id\'={bookid}'+'"}':
     print('Złe ID książki!')
@@ -47,7 +46,7 @@ if rget == '{"'+f'error":"Couldn\'t find Book with \'id\'={bookid}'+'"}':
 pages = json.loads(rget).get('pages')
 name = json.loads(rget).get('name').replace('/','')
 for page in pages:
-    if not os.path.exists(f'{path}/{name}-{bookid}/{page}'):
+    if not os.path.exists(f'{path}/{name}-{bookid}/{page}.html'):
         seconds=random.randint(2,8)
         download_page(token, page, bookid)
         print(f'Pobrano stronę {page}\nNastępna strona zostanie pobrana za {seconds} sekund')
